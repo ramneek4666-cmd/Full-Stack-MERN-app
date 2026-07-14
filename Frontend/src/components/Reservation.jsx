7d160bb405c9a421ebdb66662c0a712acbde9bd5
ramneek4666-cmd <ramneek4666@gmail.com>
2026-07-14 15:58:31 +0530

    Update Reservation.jsx

--- Frontend/src/components/Reservation.jsx
+++ Frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/reservation/send",
+        "https://mern-restaurant-backend.onrender.com/api/v1/reservation/send",
         { firstName, lastName, email, date, time, phone },
         {
           headers: {
