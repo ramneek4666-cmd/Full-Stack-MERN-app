44bccc543d62f7b10a7c6f14eaae57be350f62d5
ramneek4666-cmd <ramneek4666@gmail.com>
2026-07-14 16:35:01 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,22 +1,33 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv'
-import { dbconnection } from './Database/Dbconnection.js';
-import { errorMiddleware } from './Error/error.js';
-import ReservationRouter from "./routes/reservationRoute.js"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { dbconnection } from "./Database/Dbconnection.js";
+import { errorMiddleware } from "./Error/error.js";
+import ReservationRouter from "./routes/reservationRoute.js";
 
-const app = express()
-dotenv.config({path: "./config/config.env"})
-app.use(cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["POST"],
-    credentials:true,
-}));
+const app = express();
+
+dotenv.config({ path: "./config/config.env" });
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully!");
+});
+
 app.use("/api/v1/reservation", ReservationRouter);
-dbconnection()
+
+dbconnection();
+
 app.use(errorMiddleware);
 
 export default app;
