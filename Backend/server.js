6d0a557f57c4d67327567050c28d3aa69a94d94a
ramneek4666-cmd <ramneek4666@gmail.com>
2026-07-14 15:52:39 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,5 @@
-import app from "./app.js";
-app.listen(process.env.PORT, ()=>{
-    console.log(`server running on port ${process.env.PORT}`)
-})
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
